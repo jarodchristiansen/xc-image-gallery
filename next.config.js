eb0168096bc937fe0cabe6d455122ed0308c71a3
Jarod Christiansen <jarodchristiansen2@gmail.com>
2023-01-13 17:12:02 -0700

    csp policy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
 
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), microphone=()",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "style-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com https://*.icomoon.io; script-src 'self' *.googletagmanager.com *.tradingview.com 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com https://hodl-watch.vercel.app/ *.hodl-watch.vercel.app/",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
   images: {
     //TODO: Update this to allow more domains/figure out wildcard strategy
     domains: [
